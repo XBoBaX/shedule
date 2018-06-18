459ad9a7cc343da387bd57745153528e31d4846e
Vladimir <memesHipe@gmail.com>
2018-06-18 22:41:59 +0300

    21

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
-import telebot
-import config  # подключаем конфиг, чтобы взять с него токен бота
+# import telebot
+# import config  # подключаем конфиг, чтобы взять с него токен бота
+#
+# bot = telebot.TeleBot(config.TOKEN)
+# print(bot.get_me())
 
-bot = telebot.TeleBot(config.TOKEN)
-print(bot.get_me())
+from model import BotHandler
+import config
+
+bot = BotHandler(config.TOKEN)
--- view.py
+++ view.py
@@ -2,33 +2,33 @@
 # import datetime
 # import cherrypy
 
-token = "604891132:AAF4m5OiqhZ_gLj-3QizE84dYL2Lgfl-yvk"
-import telebot
-import os
-from flask import Flask, request
-
-bot = telebot.TeleBot(token)
-server = Flask(__name__)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-
-@server.route("/{0}".format(token), methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://shedule0server.herokuapp.com/{0}".format(token))
-    return "!", 200
-
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+# token = "604891132:AAF4m5OiqhZ_gLj-3QizE84dYL2Lgfl-yvk"
+# import telebot
+# import os
+# from flask import Flask, request
+#
+# bot = telebot.TeleBot(token)
+# server = Flask(__name__)
+#
+#
+# @bot.message_handler(commands=['start'])
+# def start(message):
+#     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+#
+# @bot.message_handler(func=lambda message: True, content_types=['text'])
+# def echo_message(message):
+#     bot.reply_to(message, message.text)
+#
+#
+# @server.route("/{0}".format(token), methods=['POST'])
+# def getMessage():
+#     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+#     return "!", 200
+#
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url="https://shedule0server.herokuapp.com/{0}".format(token))
+#     return "!", 200
+#
+# server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
