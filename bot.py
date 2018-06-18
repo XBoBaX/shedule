0dc42662a1541363b1a6c967641266e161287556
Vladimir <memesHipe@gmail.com>
2018-06-18 22:54:35 +0300

    22

--- bot.py
+++ bot.py
@@ -1,10 +1,5 @@
-# import telebot
-# import config  # подключаем конфиг, чтобы взять с него токен бота
-#
-# bot = telebot.TeleBot(config.TOKEN)
-# print(bot.get_me())
+import telebot
+import config  # подключаем конфиг, чтобы взять с него токен бота
 
-from model import BotHandler
-import config
-
-bot = BotHandler(config.TOKEN)
+bot = telebot.TeleBot(config.TOKEN)
+print(bot.get_me())
