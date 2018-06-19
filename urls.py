26979f1e97670c700d96519bf786cef2ab23d1a7
Vladimir <memesHipe@gmail.com>
2018-06-19 19:23:53 +0300

    28

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
 from config import *
 
-API_URL = "https://artncoprojects.000webhostapp.com/Raspisanie/api/{0}".format(TOKEN)
+API_URL = "https://artncoprojects.000webhostapp.com/Raspisanie/api/{0}".format(SECURITY_API)
 
-TEACHER_LIST = "{0}/teacherList".format(SECURITY_API)
-TEACHER_LIST_UKR = "{0}/teacherListUkr".format(SECURITY_API)
+TEACHER_LIST = "{0}/teacherList".format(API_URL)
+TEACHER_LIST_UKR = "{0}/teacherListUkr".format(API_URL)
 
-TEACHER_SCHEDULE = "{0}/teacherSchedule/all".format(SECURITY_API)
-STUDENT_SHEDULE = "{0}/schedule/all".format(SECURITY_API)
+TEACHER_SCHEDULE = "{0}/teacherSchedule/all".format(API_URL)
+STUDENT_SHEDULE = "{0}/schedule/all".format(API_URL)
 
-STUDENT_LIST = "{0}/groupList".format(SECURITY_API)
+STUDENT_LIST = "{0}/groupList".format(API_URL)
 
