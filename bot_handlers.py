4b089b3626191f620b1cee0c81dd547aa4599200
Vladimir <memesHipe@gmail.com>
2018-06-19 19:43:03 +0300

    28

--- bot_handlers.py
+++ bot_handlers.py
@@ -29,12 +29,18 @@ def keyboard_start(id):
     keyboard.row('1 курс', '2 курс', '3 курс')
     keyboard.row('4 курс', '5 курс', '6 курс')
     keyboard.row('7 курс', 'Преподаватели')
+
+    # msg = bot.send_message(message.from_user.id, "Выберите нужный курс", reply_markup=keyboard)
+    # bot.register_next_step_handler(msg, checkGroup)
+    print(id)
+
     keyboard_msg = bot.send_message(id, "Выберите нужный курс", reply_markup=keyboard)
     bot.register_next_step_handler(keyboard_msg, select_group)
 
 
 def select_group(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    print("111111111")
     print(message.text)
     group_list = json.loads(r.get("STUDENT_LIST"))
     print(group_list)
